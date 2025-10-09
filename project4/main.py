855ef8e0cfead97af88bb683588fdea6db9f5dec
Arpita Shanbhag <axshanbhag@zonemail.clpccd.edu>
2025-10-08 21:04:55 -0700

    Update main.py

--- project4/main.py
+++ project4/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import GWCutilities as util
-
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import export_text
+# all columns and content is shown
 pd.set_option('display.max_columns', None)
 pd.set_option('max_colwidth', None)
 
@@ -22,8 +24,16 @@ input("\n Press Enter to continue.\n")
 
 #Data Cleaning
 #Label encode the dataset
-df = util.labelEncoder(df, ["HeartDisease", "GenHealth","Smoking", "AlcoholDrinking", "PhysicalActivity", "AgeCategory"])
 
+le = LabelEncoder()
+
+# List of columns you want to encode
+cols_to_encode = ["HeartDisease", "GenHealth", "Smoking", "AlcoholDrinking", "PhysicalActivity", "AgeCategory"]
+
+# Apply encoding to each column
+# label encoding when order matters
+for col in cols_to_encode:
+    df[col] = le.fit_transform(df[col])
 
 print("\nHere is a preview of the dataset after label encoding. \n")
 print(df.head())
@@ -31,8 +41,8 @@ print(df.head())
 input("\nPress Enter to continue.\n")
 
 #One hot encode the dataset
-df = util.oneHotEncoder(df, ["Sex", "Race"])
-
+# for binary things
+df = pd.get_dummies(df, columns=["Sex", "Race"], drop_first=False)
 print(
     "\nHere is a preview of the dataset after one hot encoding. This will be the dataset used for data analysis: \n"
 )
@@ -45,11 +55,12 @@ input("\nPress Enter to continue.\n")
 
 #Creates and trains Decision Tree Model
 from sklearn.model_selection import train_test_split
+# X is all of the columns excpet target
 X = df.drop("HeartDisease", axis = 1)
-y = df["HeartDisease"]
-
+y = df["HeartDisease"] # target column of 0/1
+# data is split into training and desting sets
 X_train, X_test, y_train, y_test = train_test_split(X,y, random_state = 1)
-
+# depth is limited to prevent overfitting, fits tree on the trained values
 from sklearn.tree import DecisionTreeClassifier
 clf = DecisionTreeClassifier(max_depth = 10, class_weight = 'balanced')
 clf = clf.fit(X_train, y_train)
@@ -78,6 +89,7 @@ print(cm)
 train_predictions = clf.predict(X_train)
 
 from sklearn.metrics import accuracy_score
+# compares predicted vs actual labels
 train_acc = accuracy_score(y_train, train_predictions)
 print("The accuracy with the testing data set of the Decision Tree is :" + str(train_acc))
 
@@ -97,4 +109,6 @@ print("You can use decision trees when you need to make decisions that involve a
 #Prints a text representation of the Decision Tree
 print("\nBelow is a text representation of how the Decision Tree makes choices:\n")
 input("\nPress Enter to continue.\n")
-#util.printTree(clf, X.columns)
+# prints out a rule-based tree
+tree_rules = export_text(clf, feature_names=list(X.columns))
+print(tree_rules)
